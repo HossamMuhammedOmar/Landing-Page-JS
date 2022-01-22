d536bcd2464cfcbce17e880f6bf3ba5c9578d13e
HossamMuhammedOmar <hossamomar9876@gmail.com>
2022-01-22 09:51:51 +0200

    add smooth scroll to the nav items, create section4 and section5 in HTML file

--- js/app.js
+++ js/app.js
@@ -7,9 +7,15 @@ const navbarList = document.querySelector("#navbar__list");
 const fragmentList = document.createDocumentFragment();
 
 /**
- * Define a variable to select all sections we have it will return a (Node List),
- * And then Loop over them with the for of loop,
- * And then create li for each section, and append a link to each li,
+ * Define a variable to select all sections we have => it will return a (Node List),
+ * And then Loop over them with the for of loop.
+ * And then create 'li' for each section.
+ * And then create a link 'a' for each list item, and do some steps with this links.
+ **** 1 - Add the text to it (section name).
+ **** 2 - Add css class to it.
+ **** 3 - Add href value (section id).
+ **** 4 - Make a smooth scroll to each section.
+ * After that append link 'a' to the list item 'li'.
  * And then append these sections to a DocumentFragment,
  * Finally after the for loop, append sections to the navbarList element.
  */
@@ -17,12 +23,27 @@ const sections = document.querySelectorAll("section");
 for (let section of sections) {
   let list = document.createElement("li");
   let link = document.createElement("a");
+  // Set link text
   link.text = section.dataset.nav;
+
+  // Add css class to each link element
   link.classList.add("menu__link");
+
+  // Add href to each link element
+  link.href = `#${section.getAttribute("id")}`;
+
+  // Smooth scroll
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  });
+
   list.appendChild(link);
   fragmentList.appendChild(list);
-  console.log(fragmentList);
 }
 
-// Append fragment that is contain the list item to the navbarList
+// Append fragment (that is contain the list items) to the navbarList
 navbarList.appendChild(fragmentList);
